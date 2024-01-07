924a7a1894a17f0f03ec8e16258a33b536d561cc
Lesiba Moshweu <moshweutipe@gmail.com>
2024-01-07 09:07:43 +0200

    Init

---
+++ docs/script.js
@@ -0,0 +1,25 @@
+const div_dev_contentElement = document.getElementById('developer');
+
+const dev_contentElement = '<p> Powered by: Job Cafe   | '+ new Date().getFullYear() + ' | &copy; Copyright reserved<p/>'
+
+div_dev_contentElement.innerHTML += dev_contentElement
+
+
+// DISABLE DEV TOOL
+// document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+// function ctrlShiftKey(e, keyCode) {
+//   return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+// }
+
+// document.onkeydown = (e) => {
+//   // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+//   if (
+//     event.keyCode === 123 ||
+//     ctrlShiftKey(e, 'I') ||
+//     ctrlShiftKey(e, 'J') ||
+//     ctrlShiftKey(e, 'C') ||
+//     (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+//   )
+//     return false;
+// };
